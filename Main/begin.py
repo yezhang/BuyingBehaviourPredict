c386930c6be3eb1e894233c279bfafe627f80cde
yezhang <yezhang989@163.com>
2014-03-23 23:29:25 +0800

    Add Data Files

--- Main/begin.py
+++ Main/begin.py
@@ -2,14 +2,32 @@
 __author__ = 'better'
 
 import Orange
+from numpy import *
+from numpy.linalg.linalg import *
+
 # 导入测试数据voting.tab
 # data = orange.ExampleTable("voting")
-filepath = "D:\\t_alibaba_data.csv"
-data = Orange.data.Table(filepath)
+filepath = "C:\\Users\\better\\PycharmProjects\\BuyingBehaviourPredict\\data\\t_useritemcount.txt"
+#data = Orange.data.Table(filepath)
 
 # 输出
-all_data = len(data)
+#all_data = len(data)
+#print all_data
 
-for d in data[:100]:
-    print d
+# 构造用户矩阵，用户数884，品牌数9531
+# 用户矩阵应为9537×884，为了编程方便，先构造884×9537矩阵
+userNum = 884
+brandNum = 9531
+
+userMatrix = zeros((userNum,brandNum))
+print userMatrix
+userMatrix = matrix('1,2,3;4,5,6')
+# userMatrix = zeros((2,3))
+userMatrix[0,0] = 123
+userMatrix[1,2] = 456
+
+U, s, V = svd(userMatrix, full_matrices=True)
+print U
+print s
+print V
 
